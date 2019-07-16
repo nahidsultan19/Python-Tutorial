aff4bef9abf03379a8a7a14547183acdd12da896
Nahid Sultan <nahidsultan38@gmail.com>
2019-07-16 17:55:43 +0800

    Clock with Bangladeshi Flag

--- DigitalClock.py
+++ DigitalClock.py
@@ -2,14 +2,14 @@ import sys
 from tkinter import *
 import time
 
-def tick():
+def times():
 	now_time = time.strftime('%H:%M:%S')
 	clock.config(text = now_time)
-	clock.after(200, tick)
+	clock.after(200, times)
 
 
 root = Tk()
 clock = Label(root,font= ('times',100,'bold'),fg='red',bg='green')
 clock.grid()
-tick()
+times()
 root.mainloop()
