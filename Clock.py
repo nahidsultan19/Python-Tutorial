efa64bcfbf93c6c18872ab389052b5dfefc8e1e7
Nahid <nahidsultan38@gmail.com>
2020-04-27 14:05:17 +0600

    Update April 27 2020

---
+++ Clock.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import sys
+import time
+
+app = Tk()
+
+clock = Label(app, font=('times 100 bold'), fg='red', bg='green')
+clock.grid(row=0, column=0)
+
+def times():
+    now_time = time.strftime('%H:%M:%S')
+    clock.config(text=now_time)
+    clock.after(200, times)
+
+
+times()
+
+
+app.title('Digital Clock')
+app.mainloop()
--- DigitalClock.py
+++ DigitalClock.py
@@ -2,14 +2,17 @@ import sys
 from tkinter import *
 import time
 
+app = Tk()
+clock = Label(app,font= ('times',100,'bold'),fg='red',bg='green')
+clock.grid()
+
 def times():
 	now_time = time.strftime('%H:%M:%S')
 	clock.config(text = now_time)
 	clock.after(200, times)
 
 
-root = Tk()
-clock = Label(root,font= ('times',100,'bold'),fg='red',bg='green')
-clock.grid()
+
 times()
-root.mainloop()
+app.title('Digital Clock')
+app.mainloop()
